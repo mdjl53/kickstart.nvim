4fab5090d9651c22e29c40a73aa25faf3cfaf9da
mdjl53 <michael@slowflow.com>
2025-04-17 16:00:13 -0400

    nvim working - updating submodule

---
+++ lua/custom/themes/doomone.lua
@@ -0,0 +1,17 @@
+-- Optional settings (customize as desired)
+vim.g.doom_one_terminal_colors = true
+vim.g.doom_one_enable_treesitter = true
+vim.g.doom_one_diagnostics_text_color = true
+
+-- Plugins integration (optional)
+vim.g.doom_one_plugin_neorg = true
+vim.g.doom_one_plugin_barbar = false
+vim.g.doom_one_plugin_telescope = true
+vim.g.doom_one_plugin_neogit = false
+vim.g.doom_one_plugin_nvim_tree = true
+vim.g.doom_one_plugin_dashboard = false
+vim.g.doom_one_plugin_startify = false
+
+-- Apply the colorscheme
+vim.cmd.colorscheme 'doom-one'
+
---
+++ lua/custom/themes/gruvbox.lua
@@ -0,0 +1,9 @@
+-- Set the variant you want (uncomment to activate)
+vim.g.gruvbox_material_background = 'medium' -- 'soft', 'medium', or 'hard'
+vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_enable_italic = 1
+vim.g.gruvbox_material_transparent_background = 0
+
+-- Apply the colorscheme
+vim.cmd.colorscheme 'gruvbox-material'
+
